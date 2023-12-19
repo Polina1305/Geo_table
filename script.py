66ffd2e74130b950a8317bdc748e81cb2623f745
Smol_Pol <106150320+Polina1305@users.noreply.github.com>
2023-12-19 19:41:47 +0300

    Update script.py

--- script.py
+++ script.py
@@ -6,6 +6,20 @@ from psycopg2 import OperationalError
 
 
 def create_connection(name, user, password, host, port, database):
+    """
+    Создает подключение к базе данных PostgreSQL.
+    
+    Args:
+        name (str): Название драйвера.
+        user (str): Имя пользователя.
+        password (str): Пароль.
+        host (str): Хост.
+        port (int): Порт.
+        database (str): Название базы данных.
+        
+    Returns:
+        engine: Объект для выполнения SQL-запросов.
+    """
     engine = None
     try: 
         DATABASE = {
@@ -20,12 +34,23 @@ def create_connection(name, user, password, host, port, database):
 
         engine = create_engine(URL(**DATABASE))
         print("Connection to PostgreSQL DB successful")
-    except:
+    except :
         print("The error")
     return engine 
 
 
 def query_table(engine, model_id):
+    """
+    Выполняет SQL-запрос для извлечения данных из таблицы в базе данных.
+    Затем применяет модель SentenceTransformer для векторизации значений столбца "name" и сохраняет результат в новую таблицу.
+    
+    Args:
+        engine: Объект для выполнения SQL-запросов.
+        model_id (str): Идентификатор модели SentenceTransformer.
+    
+    Returns:
+        tuple: Возвращает имена, векторные представления и объект модели SentenceTransformer.
+    """
     query = '''SELECT
                       DISTINCT(city.geonameid),
                       city.name,
@@ -50,12 +75,34 @@ def query_table(engine, model_id):
     return names, embeddings, labse
 
 def find_similar_labse(geoname, names, embeddings, model, top_k):
+    """
+    Находит наиболее похожие значения на заданное значение с помощью модели SentenceTransformer.
+    
+    Args:
+        geoname (str): Заданное значение для поиска.
+        names (list): Список всех значений из столбца "name".
+        embeddings: Векторные представления значений столбца "name".
+        model: Объект модели SentenceTransformer.
+        top_k (int): Количество наиболее похожих значений, которые нужно вывести.
+    
+    Returns:
+        DataFrame: Индексы и значения наиболее похожих элементов.
+    """
     response = pd.DataFrame(util.semantic_search(query_embeddings= model.encode(geoname), corpus_embeddings=embeddings, top_k=top_k)[0])
     cos_sim = response.assign(name=names[response.corpus_id])
     return  cos_sim
 
 def result_model(cos_sim, engine):
-     
+    """
+    Формирует результат запроса с информацией о наиболее похожих значениях и их косинусной схожести.
+    
+    Args:
+        cos_sim (DataFrame): Индексы и косинусная схожесть наиболее похожих значений.
+        engine: Объект для выполнения SQL-запросов.
+    
+    Returns:
+        list: Список словарей с информацией о наиболее похожих значениях.
+    """
     index_list = []    
     for i in cos_sim.values:
         index_list.append(i[0])  
